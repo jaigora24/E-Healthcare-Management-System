d8d0c7bce37a717e139ff395fa65e1f2dc9b6a8f
Jai Gora <Jaigora24@gmail.com>
2022-07-27 00:01:23 +0530

    Create script.js

---
+++ src/main/webapp/script.js
@@ -0,0 +1,15 @@
+function print_page(){
+	
+	var ButtonControl = document.getElementById("print-btn");
+	
+    ButtonControl.style.visibility = "hidden";
+    
+    window.print();
+    
+}
+
+function refresh(){
+	
+    window.location.reload();
+    
+}
