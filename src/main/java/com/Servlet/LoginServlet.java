5c4eae4815e6638f1b58534cb62660bcbf15db64
Jai Gora <Jaigora24@gmail.com>
2022-09-17 22:06:45 +0530

    Update LoginServlet.java

--- src/main/java/com/Servlet/LoginServlet.java
+++ src/main/java/com/Servlet/LoginServlet.java
@@ -5,6 +5,8 @@ import java.io.PrintWriter;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	String username, password;
@@ -13,18 +15,21 @@ public class LoginServlet extends HttpServlet {
 		username = req.getParameter("username");
 		password = req.getParameter("password");
 		PrintWriter out = res.getWriter();
-		// pass default "admin" as username and "admin123" as password
 		try {
 			if(username.equals("admin") && password.equals("admin123")) {
 				res.sendRedirect("AdminPortal.jsp");
 			}
 			else {
+				out.println("<h1>");
 				out.println("Incorrect Username or Password");
+				out.println("</h1");
 			}
 		}
 		catch(Exception e) {
-			out.println("Unexpected Error Occured!");
-			System.out.println(e);
+			    out.println("<h1 text-align='center'>");
+			    out.println("Some Error Occured. Please contact the development team");
+			    out.println("</h1>");
+			    System.out.println(e);
 		}
 	
 	}
