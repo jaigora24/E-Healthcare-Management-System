2cfaa43d4715713b190aebe6d492b2746112a716
Jai Gora <Jaigora24@gmail.com>
2022-09-17 22:14:38 +0530

    Update script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -2,14 +2,14 @@ function print_page(){
 	
 	var ButtonControl = document.getElementById("print-btn");
 	
-    ButtonControl.style.visibility = "hidden";
+    	ButtonControl.style.visibility = "hidden";
     
-    window.print();
+    	window.print();
     
 }
 
 function refresh(){
 	
-    window.location.reload();
+    	window.location.reload();
     
 }
